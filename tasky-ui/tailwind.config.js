3a9d9da2ae48e6b3e4e65b6a06f699c9fe1f7191
Ion Gondrea <gondrea.02@gmail.com>
2025-11-05 13:33:58 +0200

    ci: add tailwind/postcss dev deps tailwind config and CI debug step

---
+++ tasky-ui/tailwind.config.js
@@ -0,0 +1,14 @@
+// tasky-ui/tailwind.config.js
+// Minimal Tailwind config for Vite + PostCSS
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
